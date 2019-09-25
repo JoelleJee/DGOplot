8d7f109702ec582cbe171d5565a440676fe4d9c0
Joelle Jee <joelle.jee@mail.utoronto.ca>
2019-09-25 17:32:35 -0400

    added default value for OrgDb parameter for enrichDGO and debugged

--- DGOplot.R
+++ DGOplot.R
@@ -10,20 +10,20 @@ BiocManager::install("clusterProfiler")
 
 data(DO2EG)
 
-enrichDGO <- function(gene, Gont = "MF", Dont = "DO", 
+enrichDGO <- function(gene, Gont = "MF", Dont = "DO", OrgDb =  org.Hs.eg.db,
                       pvalueCutoff = 0.05, pAdjustMethod = "BH", universe,
-                      qvalueCutoff = 0.2, minGSSize = 10, maxSize = 500,
+                      qvalueCutoff = 0.2, minGSSize = 10, maxGSSize = 500,
                       readable = FALSE, pool = FALSE){
   
   # perform enrichDO and enrichGO on input of genes.
   # returns a vector of both analyses.
   
-  DOanalysis <- DOSE::enrichDO(gene, Dont, pvalueCutoff, pAdjustMethod,
-           universe, minGSSize, maxGSSize, qvalueCutoff, readable)
+  DOanalysis <- DOSE::enrichDO(gene, Dont, pvalueCutoff, pAdjustMethod, 
+                               universe, minGSSize, maxGSSize, qvalueCutoff, readable)
   
-  GOanalysis <- clusterProfiler::enrichGO(gene, OrgDb, Gont, pvalueCutoff, pAdjustMethod, universe,
-                         qvalueCutoff, minGSSize, maxGSSize,
-                         readable, pool)
+  GOanalysis <- clusterProfiler::enrichGO(gene, OrgDb, keyType = "ENTREZID", Gont, pvalueCutoff, 
+                                          pAdjustMethod, universe, qvalueCutoff, minGSSize, 
+                                          maxGSSize, readable, pool)
   
   DGOanalysis <- c(DOanalysis, GOanalysis)
   names(DGOanalysis) <- c("DO", "GO")
