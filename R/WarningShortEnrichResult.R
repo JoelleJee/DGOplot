9a34fd5944d86428185f8b9df7f1a644e0b44d18
Joelle Jee <joelle.jee@mail.utoronto.ca>
2019-12-04 11:29:05 -0500

    removed type parameter as seen unncessary; now both bar and net plots implement pvalueCutoff; changed warning message to say that ony one of DO or GO is depicted if one doesn't have any resuls

--- R/WarningShortEnrichResult.R
+++ R/WarningShortEnrichResult.R
@@ -10,32 +10,28 @@
 #'
 #' @return warning or error depending on the input
 
-warnOntN <- function(nDO, nGO, showCategory, type) {
+warnOntN <- function(nDO, nGO, showCategory) {
+  # warning message
+  warn <- c()
+  
   if (nDO == 0 & nGO == 0) {
-    if (type == "bar"){
-      stop("0 ontology groups in DGOResult.")
-    } else if (type == "net"){
-      stop("0 ontology groups in DGOResult, or below pvalueCutoff")
-    }
-  }
-  else if (nDO == 0) {
-    warn <- sprintf("0 disease ontology groups selected.\n
-                    Check pvalueCutoff if using DGOnetplot().")
-  } else if (nGO == 0) {
-    warn <- sprintf("0 gene ontology groups selected.
-                    Check pvalueCutoff if using DGOnetplot().")
+    stop("0 ontology groups in DGOResult, or below pValueCutoff.\n")
   } else if (nDO < showCategory | nGO < showCategory){
     warn <- sprintf("Input showCategory = %d \n", showCategory)
     if (nDO < showCategory){
-      warn <- sprintf("%sOnly %d Disease Ontology groups found.\n
-                      Check pvalueCutoff if using DGOnetplot().", 
+      warn <- sprintf("%s%d Disease Ontology groups found.Check pvalueCutoff.\n", 
                       warn, nDO)
+      if (nDO == 0) {
+        warn <- sprintf("%sOnly displaying GO enrichment results.\n", warn)
+      } 
     } 
     if (nGO < showCategory) {
-      warn <- sprintf("%sOnly %d Gene Ontology groups found.\n
-                      Check pvalueCutoff if using DGOnetplot().\n",
+      warn <- sprintf("%s%d Gene Ontology groups found.\nCheck pvalueCutoff.\n",
                       warn, nGO)
-    }
+      if (nGO == 0) {
+        warn <- sprintf("%sOnly displaying GO enrichment results.\n", warn)
+      }
+    } 
     warning(warn)
   } else {
     return()
